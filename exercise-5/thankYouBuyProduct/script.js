79402ad55a6c01fd365c9ebf15906811f06bee3d
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-08-19 13:45:57 +0700

    delete exercise-5

--- exercise-5/thankYouBuyProduct/script.js
+++
@@ -1,7 +0,0 @@
-function greet(nama,produk){
-    alert( "Terima kasih " + nama + " anda telah membeli produk " + produk) ;
-}
-
-let nama = prompt("masukan nama anda: ");
-let produk = prompt("masukan nama produk yang akan dibeli: ");
-console.log(greet(nama, produk));
